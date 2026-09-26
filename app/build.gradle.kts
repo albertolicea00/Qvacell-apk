77579d4e4b972b119cde63e350af07cf65fdd298
albertolicea00 <83251892+albertolicea00@users.noreply.github.com>
2026-09-26 08:59:57 -0400

    chore: customize APK output filename to include variant name and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,14 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    applicationVariants.all {
+        val variant = this
+        outputs.all {
+            val outputImpl = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            outputImpl.outputFileName = "qvacell-${variant.name}-v${variant.versionName}.apk"
+        }
+    }
 }
 
 dependencies {
